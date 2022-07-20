65e2b90562a9bf1eef1cd39b2798fcf8c9064b8e
Daniel Croft <daniel.croft@sap.com>
2022-07-20 10:18:22 +0000

    uploading to github
    git pus

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("studentcomp.controller.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
